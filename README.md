372329d0fa97e343afc22c850e8dee8aa5030666
Your Name <you@example.com>
2020-05-29 17:27:42 +0530

    commit again

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-# AdvancedNodeStarter
-Starting project for a course on Advanced Node @ Udemy
-=======
-# MERN-Stack
->>>>>>> d420a30f3dadcdc4dcc44f402a828d715cb08bf6
+#MEARN Stack Learning App
--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '283028152941-dq89lpvpg8uj2b56p3gi6k01q830on0r.apps.googleusercontent.com',
   googleClientSecret: 'puXkofJGdTWT64HE6l-Gj46x',
-  mongoURI: 'mongodb+srv://dbUser:dbUserPassword@mongodb-dev-zkjmu.mongodb.net/Bloggster-DEV?retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: 'wsedfghjkjhgdchytdctvsgudbhwvukqwd',
   redisUrl: 'redis://127.0.0.1:6379'
 };
